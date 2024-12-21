9623d89c3bfbc87e0c8e61a1027525a22fb272e2
lhdht <15229684090@163.com>
2024-12-21 19:11:26 +0800

    fix: 测试写入请求串

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"net/http"
 	"strings"
-	"time"
 )
 
 func main() {
@@ -15,12 +14,14 @@ func main() {
 	method := "POST"
 	importApiUrl := fmt.Sprintf("https://api.apifox.com/v1/projects/%s/import-openapi", config.ProjectId)
 
-	request, err := http.NewRequest(method, importApiUrl, strings.NewReader(body))
+	//_ = os.WriteFile("request.json", body, 0755)
+
+	request, err := http.NewRequest(method, importApiUrl, strings.NewReader(string(body)))
 	if err != nil {
 		panic(err)
 	}
 
-	request.Header.Add("X-Apifox-Api-Version", time.Now().Format(time.DateTime))
+	request.Header.Add("X-Apifox-Api-Version", "2024-03-28")
 	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", config.Token))
 	request.Header.Add("Content-Type", "application/json")
 
